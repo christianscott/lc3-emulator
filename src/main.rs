459f3d29c252ebefdefeece09a494e7c61d5f993
Christian Scott <christian.s@canva.com>
2019-04-28 15:56:59 +1200

    import os in main

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+mod assembler;
+mod lc3;
+
+fn main() {
+  let os = include_str!("./os.asm");
+  let os_executable = assembler::assemble(String::from(os));
+  let instructions = os_executable
+    .instructions
+    .iter()
+    .map(|instruction| lc3::Instruction::from(*instruction));
+  lc3::Machine::run(instructions.collect());
+}
