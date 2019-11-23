f625c57f9b3593bb083f0b0b0858917157114194
Christian Scott <christianfscott@gmail.com>
2019-11-23 15:40:17 +1100

    add reader for parser

--- src/assembler/parser.rs
+++ src/assembler/parser.rs
@@ -11,16 +11,85 @@ impl ParseError {
 	}
 }
 
+#[derive(Debug)]
+struct Reader {
+	tokens: Vec<Token>,
+	offset: usize,
+	char_in_line: usize,
+	line: usize,
+}
+
+impl Reader {
+	fn from(tokens: Vec<Token>) -> Self {
+		Reader {
+			tokens,
+			offset: 0,
+			char_in_line: 0,
+			line: 0,
+		}
+	}
+
+	fn get(&self, index: usize) -> Option<Token> {
+		self.tokens.get(index).map(ToOwned::to_owned)
+	}
+
+	fn peek(&self) -> Option<Token> {
+		self.get(self.offset)
+	}
+
+	fn next(&mut self) -> Option<Token> {
+		let c = self.peek();
+		self.offset += 1;
+
+		if c.clone().map_or(false, |c| c == Newline) {
+			self.line += 1;
+			self.char_in_line = 0;
+		} else {
+			self.char_in_line += 1;
+		}
+
+		c
+	}
+
+	fn skip_while<F>(&mut self, predicate: F)
+	where
+		F: Fn(Token) -> bool + Copy,
+	{
+		while self.peek().map_or(false, predicate) {
+			self.next();
+		}
+	}
+
+	fn take_while<F>(&mut self, predicate: F) -> Vec<Token>
+	where
+		F: Fn(Token) -> bool + Copy,
+	{
+		let mut chars = Vec::new();
+		while self.peek().map_or(false, predicate) {
+			match self.next() {
+				Some(c) => chars.push(c),
+				None => break,
+			}
+		}
+		chars.iter().map(ToOwned::to_owned).collect()
+	}
+}
+
 type Instruction = u16;
 
-struct Parser {}
+struct Parser {
+	reader: Reader,
+}
 
 impl Parser {
 	fn parse(tokens: Vec<Token>) -> Result<Vec<Instruction>, ParseError> {
-		for token in tokens {
+		let mut parser = Parser {
+			reader: Reader::from(tokens),
+		};
+		while let Some(token) = parser.reader.next() {
 			match token {
 				Directive(directive) => {
-					parse_directive(&directive)?;
+					parser.parse_directive(&directive)?;
 					continue;
 				}
 				Symbol(_string) => continue,
@@ -33,19 +102,19 @@ impl Parser {
 
 		Ok(vec![])
 	}
-}
 
-fn parse_directive(directive: &str) -> Result<(), ParseError> {
-	match directive.to_lowercase().as_ref() {
-		".fill" => {}
-		_ => {
-			return Err(ParseError {
-				message: format!("unrecognized directive: {}", directive),
-			})
+	fn parse_directive(&mut self, directive: &str) -> Result<(), ParseError> {
+		match directive.to_lowercase().as_ref() {
+			".fill" => {}
+			_ => {
+				return Err(ParseError {
+					message: format!("unrecognized directive: {}", directive),
+				})
+			}
 		}
-	}
 
-	Ok(())
+		Ok(())
+	}
 }
 
 pub fn parse(tokens: Vec<Token>) -> Result<Vec<u16>, ParseError> {
