06da7392fc7a958e6c0ad212b585f49fc7b9bc5f
Christian Scott <christianfscott@gmail.com>
2019-11-23 12:19:11 +1100

    add basic parser

--- src/assembler/parser.rs
+++ src/assembler/parser.rs
@@ -1,15 +1,71 @@
-use crate::{assembler::lexer::Token, instructions::Instruction};
+use crate::assembler::lexer::Token::{self, *};
 
-enum ParseState {
-    Initial,
+#[derive(Debug, PartialEq)]
+pub struct ParseError {
+	pub message: String,
 }
 
-pub fn parse(tokens: Vec<Token>) -> Result<Vec<Instruction>, String> {
-    let _state = ParseState::Initial;
-    for _token in tokens {}
+impl ParseError {
+	pub fn pretty(self) -> String {
+		String::from("")
+	}
+}
 
-    Ok(Default::default())
+type Instruction = u16;
+
+struct Parser {}
+
+impl Parser {
+	fn parse(tokens: Vec<Token>) -> Result<Vec<Instruction>, ParseError> {
+		for token in tokens {
+			match token {
+				Directive(directive) => {
+					parse_directive(&directive)?;
+					continue;
+				}
+				Symbol(_string) => continue,
+				Number(_num) => continue,
+				Comma => continue,
+				Str(_string) => continue,
+			}
+		}
+
+		Ok(vec![])
+	}
+}
+
+fn parse_directive(directive: &str) -> Result<(), ParseError> {
+	match directive.to_lowercase().as_ref() {
+		".fill" => {}
+		_ => {
+			return Err(ParseError {
+				message: format!("unrecognized directive: {}", directive),
+			})
+		}
+	}
+
+	Ok(())
+}
+
+pub fn parse(tokens: Vec<Token>) -> Result<Vec<u16>, ParseError> {
+	Parser::parse(tokens)
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+	use super::*;
+
+	fn directive(d: &str) -> Token {
+		Directive(String::from(d))
+	}
+
+	#[test]
+	fn test_bad_directive() {
+		assert_eq!(
+			parse(vec![directive(".bad")]),
+			Err(ParseError {
+				message: String::from("unrecognized directive: .bad")
+			}),
+		);
+	}
+}
